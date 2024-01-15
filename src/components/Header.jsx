dc5cfbbc4294f0b5d515d9127ee73534b3e4a8f0
sepideh nilforoushan <3pd.nilforoushan@gmail.com>
2024-01-15 12:24:13 +0330

    using css module instead of css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import "../assets/styles/header.css";
+import clasees from "../assets/styles/Header.module.css";
 import logo from "../assets/images/logo.jpg";
 import Button from "./UI/Button";
 import { useContext } from "react";
@@ -10,12 +10,12 @@ export default function Header() {
     return totalItemsOfCart + item.quantity;
   }, 0);
   return (
-    <header className="main-header">
-      <div className="header-title">
-        <img src={logo} className="header-logo" />
+    <header className={clasees["main-header"]}>
+      <div className={clasees["header-title"]}>
+        <img src={logo} className={clasees["header-logo"]} />
         <h1 className="headerName">ReactFood</h1>
       </div>
-      <nav className="cart">
+      <nav className={clasees["cart"]}>
         <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
